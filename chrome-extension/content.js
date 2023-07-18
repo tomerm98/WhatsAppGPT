9e565fd92696aff8185700c88239a6ac26e11cbb
Tomer Mardan <tomer.mardan@gmail.com>
2023-07-18 12:41:32 +0300

    tmp

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,11 +1,18 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // Wait until the page is fully loaded
-  window.onload = function() {
-    // Find the textbox and paste the processed text into it
-      console.log("TESTINNGGGG CONTENT JSS ***************")
-    let textbox = document.querySelector('input[type="text"]'); // Update this selector based on the actual textbox
-    if (textbox) {
-      textbox.value = request.processedText;
-    }
-  }
-});
+window.onload = function() {
+  console.log('LOADED ****')
+  setInterval(() => navigator.clipboard.readText().then(console.log), 1000)
+}
+
+
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   // Wait until the page is fully loaded
+//   window.onload = function() {
+//     // Find the textbox and paste the processed text into it
+//       console.log("TESTINNGGGG CONTENT JSS ***************")
+//     let textbox = document.querySelector('input[type="text"]'); // Update this selector based on the actual textbox
+//     if (textbox) {
+//       textbox.value = request.processedText;
+//     }
+//   }
+// });
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,31 +1,38 @@
 document.getElementById('process-btn').addEventListener('click', () => {
   console.log("button clicked")
-  let spinner = document.getElementById('spinner');
-  let btn = document.getElementById('process-btn');
+  const spinner = document.getElementById('spinner');
+  const btn = document.getElementById('process-btn');
 
   btn.style.display = 'none';
   spinner.style.display = 'block';
 
-  // Read the clipboard data
-  navigator.clipboard.readText()
-    .then((text) => {
-      console.log(`clipboard is: ${text}`)
-      processText(text).then((processedText) => {
-        console.log("Text has been processed")
-        // Open a new tab with a specific URL
-        chrome.tabs.create({ url: 'http://www.yourwebsite.com' }, function(tab) {
-          // We still need a content script to handle injecting the text into the page
-          console.log("Sending tab message")
-          chrome.tabs.sendMessage(tab.id, {processedText: processedText});
-        });
+  
 
-        btn.style.display = 'block';
-        spinner.style.display = 'none';
-      });
-    });
+  // Read the clipboard data
+//   console.log(navigator.clipboard)
+//   navigator.clipboard.readText()
+//     .then((text) => {
+//       console.log(`clipboard is: ${text}`)
+//       // Open a new tab with a specific URL
+//       chrome.tabs.create({ url: 'http://www.yourwebsite.com' }, function(tab) {
+//         // We still need a content script to handle injecting the text into the page
+//         console.log("Sending tab message")
+//         chrome.tabs.sendMessage(tab.id, {processedText: processedText});
+//       });
+//
+//       btn.style.display = 'block';
+//       spinner.style.display = 'none';
+//     });
+
+
 });
 
-// Process text from clipboard
-async function processText(text) {
-  return text;
+const readClipboard = () =>{
+  const placeholder = document.createElement("input");
+  document.body.appendChild(placeholder);
+  placeholder.focus();
+  document.execCommand("paste");
+  const clipboardText = placeholder.value;
+  document.body.removeChild(placeholder);
+  return clipboardText;
 }
